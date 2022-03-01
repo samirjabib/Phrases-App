2cb07ec6a38ab0e06101c22d5129d1b1a5b1a418
samir <samir.jc123@gmail.com>
2022-02-28 20:01:57 -0500

    segundos cambios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Quotes from './components/Quotes';
 
 function App() {
   return (
     <div className="App" >
-      <Quotes/>
-      
+      <Quotes/>  
     </div>
   );
 }
